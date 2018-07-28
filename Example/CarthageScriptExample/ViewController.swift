db4eab98ff576d76ad689802f7af63eeb2670f4c
k-lpmg <kanglpmg@gmail.com>
2018-07-29 06:26:53 +0900

    Remove annotation

--- Example/CarthageScriptExample/ViewController.swift
+++ Example/CarthageScriptExample/ViewController.swift
@@ -1,12 +1,5 @@
-//
-//  ViewController.swift
-//  CarthageScriptExample
-//
-//  Created by DongHeeKang on 2018. 5. 23..
-//  Copyright © 2018년 k-lpmg. All rights reserved.
-//
-
 import UIKit
+
 import SwiftyJSON
 import SVProgressHUD
 
